3da6b0f65012a537dda8476aaf758e7afb9d0e36
Jesper Söderborg <jesper.soderborg@gmail.com>
2025-10-09 14:26:47 +0200

    Small changes

---
+++ src/main/java/net/laisvall/perspectivemanager/client/ui/PerspectiveOverlay.java
@@ -0,0 +1,16 @@
+package net.laisvall.perspectivemanager.client.ui;
+
+public class PerspectiveOverlay {
+//    @SubscribeEvent
+//    public void onRenderOverlay(RenderGameOverlayEvent.Pre event) {
+//        // Cancel vanilla HUD parts you don’t want
+//        if (event.getType() == RenderGameOverlayEvent.ElementType.ALL ||
+//                event.getType() == RenderGameOverlayEvent.ElementType.CROSSHAIRS ||
+//                event.getType() == RenderGameOverlayEvent.ElementType.HOTBAR ||
+//                event.getType() == RenderGameOverlayEvent.ElementType.HEALTH ||
+//                event.getType() == RenderGameOverlayEvent.ElementType.ARMOR) {
+//            event.setCanceled(true);
+//        }
+//    }
+
+}
